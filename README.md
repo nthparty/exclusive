759bd8b9fb20ade996e9d7b9f0d456e22c279d73
Andrei Lapets <a@lapets.io>
2022-05-23 22:50:58 -0400

    Initial module (with doctests), README, linting rules, and package files.

--- README.md
+++
@@ -1,2 +0,0 @@
-# exclusive
-Data structure for representing secret shares of byte vectors based on bitwise XOR, designed for use within secure multi-party computation (MPC) protocol implementations.
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
+
+# The lines below can be parsed by `docs/conf.py`.
+name = "exclusive"
+version = "0.1.0"
+
+setup(
+    name=name,
+    version=version,
+    packages=[name,],
+    install_requires=[],
+    license="MIT",
+    url="https://github.com/nthparty/exclusive",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Data structure for representing secret shares of byte " + \
+                "vectors based on bitwise XOR, designed for use within " + \
+                "secure multi-party computation (MPC) protocol " + \
+                "implementations.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+)
+ 
